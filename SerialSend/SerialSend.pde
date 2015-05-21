34b787b47bc92623f3e2274da5c8600573b8121e
NWChen <neilwchen@gmail.com>
2015-05-21 14:29:42 -0400

    shortened

---
+++ SerialSend/SerialSend.pde
@@ -0,0 +1,70 @@
+import processing.serial.*;
+//import processing.font.*;
+
+Serial port;
+
+void setup() {
+  
+  size(300,300);
+  
+  println("active go ahead");
+  
+  println(Serial.list());
+  
+  port = new Serial(this, Serial.list()[0], 9600);
+}
+
+void draw()
+{
+if( keyPressed) {
+    if(key == CODED) 
+    {
+      if (keyCode == LEFT && keyCode != RIGHT) {
+        port.write('L');
+        println("L");
+      }
+      
+      if (keyCode == RIGHT && keyCode != LEFT) {
+        port.write('R');
+        println("R");
+      }
+      
+      if (keyCode == UP && keyCode != DOWN){
+        port.write('U');
+        println("U");
+      }
+      
+      if(keyCode == DOWN && keyCode != UP){
+        port.write('D');
+        println("D");
+      }
+
+    }
+
+    else{
+      if(key == 'w' && key != 'q'){
+        port.write('W');
+        println("W");
+      }
+      
+      if(key == 'q' && key != 'w'){
+        port.write('Q');
+        println("Q");
+      }
+      
+      if(key == 'a' && key != 's'){
+        port.write('A');
+        println("A");
+      }
+      
+      if(key == 's' && key != 'a'){
+        port.write('S');
+        println("S");
+      }
+    }    
+}
+else {
+        port.write(0);
+      }
+}
+  
---
+++ ServoMover/ServoMover.ino
@@ -0,0 +1,103 @@
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist;
+
+int position = 0;
+char incomingByte;
+
+void setup() {
+  base.attach(9);
+  shoulder.attach(10);
+  elbow.attach(11);
+  wrist.attach(8);
+  wrist.write(position);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  if (Serial.available() > 0) {
+    
+    incomingByte = Serial.read();
+    
+    if(incomingByte == 'R') {
+      base.write(110);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'L') {
+      base.write(70);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'U'){
+      shoulder.write(130);
+      delay(10); 
+      freezeAll();
+    }
+    
+    if(incomingByte == 'D'){
+      shoulder.write(70);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'W' || incomingByte == 'w'){
+      elbow.write(120);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'Q' || incomingByte == 'q'){
+      elbow.write(80);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'A' || incomingByte == 'a'){
+      position = position + 1;
+      wrist.write(position);
+      delay(10);
+      freezeAll();
+    }
+    
+    if(incomingByte == 'S' || incomingByte == 's'){
+      position = position - 1;
+      wrist.write(position);
+      delay(10);
+      freezeAll();
+    }    
+  }
+  
+  Serial.println(incomingByte);
+    delay(10);
+    
+}
+
+void freezeAllExcept(String motor){
+  if(motor=="base"){
+    shoulder.write(90);
+    elbow.write(90);
+  }
+  
+  if(motor=="shoulder"){
+    base.write(90);
+    elbow.write(90);
+  }
+  
+  if(motor=="elbow"){
+    base.write(90);
+    shoulder.write(90);
+  }
+}  
+
+void freezeAll(){
+  base.write(90);
+  shoulder.write(90);
+  elbow.write(93);
+}
